40cb31d86fabeed99ad2c3e94e5d7d31494e3483
Vinicius Celms <by.vinicius@gmail.com>
2017-01-23 16:32:03 -0200

    Corrections during tests

--- core/picture.py
+++ core/picture.py
@@ -1,17 +1,29 @@
 from PIL import Image
 import os, sys
 
+class Dimensions:
+
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    def new_height(self):
+        return int((self._height / self._width) * 1920)
+
+    def new_width(self):
+        return int((self._width / self._height) * 1080)
+
 class Picture:
     def __init__(self, file, new_directory=None):
         if os.path.isfile(file):
             self.path, self.file = os.path.split(os.path.abspath(file)) # Separate path from filename
             self.file, self.extension = os.path.splitext(self.file) # Separate file name from extension
-            self._size_before = image_size()
-            self._readable_size_before = readable_size(self.size_before)
+            self._size_before = self.image_size(self.full_file_path())
+            self._readable_size_before = self.readable_size(self._size_before)
             self._size_after = None
             self._readable_size_after = None
             
-            self.im = Image.open(full_file_path()) # Open image object to get dimensions of file
+            self.im = Image.open(self.full_file_path()) # Open image object to get dimensions of file
             self._width_before, self._height_before = self.im.size
             self._width_after, self._height_after = None, None
 
@@ -19,7 +31,7 @@ class Picture:
 
             self._situation, self._condition = None, None
         else:
-            raise TypeError
+            self.file = None
 
     @property
     def size_before(self):
@@ -53,8 +65,8 @@ class Picture:
     def height_after(self):
         return self._height_after
 
-    def image_size(self):
-        return os.stat(full_file_path()).st_size
+    def image_size(self, file):
+        return os.stat(file).st_size
 
     def full_file_path(self):
         return "{}{}{}{}".format(self.path, os.path.sep, self.file, self.extension)
@@ -63,7 +75,7 @@ class Picture:
         if not self._new_directory:
             return "{}{}{}_resized{}".format(self.path, os.path.sep, self.file, self.extension)
         else:
-            return "{}{}{}{}{}_resized{}".format(self.path, os.path.sep, self._new_directory, os.path.sep self.file, self.extension)
+            return "{}{}{}{}{}_resized{}".format(self.path, os.path.sep, self._new_directory, os.path.sep, self.file, self.extension)
 
     def readable_size(self, num):
         for x in ['bytes','KB','MB','GB']:
@@ -73,36 +85,54 @@ class Picture:
         return "{:3.1f}TB".format(num)
 
     def resize_picture(self):
-        if new_directory:
-            os.mkdir("{}{}resized".format(self.path, os.path.sep))
+        if self.file:
+            if self._new_directory and not os.path.isdir("{}{}{}".format(self.path, os.path.sep, self._new_directory)):
+                    os.mkdir("{}{}{}".format(self.path, os.path.sep, self._new_directory))
 
-        if self._width_before < 1920 and self._height_before < 1080:
-            self._situation = "It will not change"
-            self._condition = "Dimensions below standard (Full HD)"
-        elif self._width_before == 1920 or self._height_before == 1080:
-            self._situation = "It will not change"
-            self._condition = "Dimensions are already standardized"
-        else:
-            d = Dimensions(self._width_before, self._height_before)
-
-            if (self._width_before - 1920) > (self._height_before - 1080):
-                self.im = self.im.resize((d.new_width(), 1080))
+            if self._width_before < 1920 and self._height_before < 1080:
+                self._situation = "It will not change"
+                self._condition = "Dimensions below standard (Full HD)"
+                self._width_after = self._width_before
+                self._height_after = self._height_before
+                self._size_after = self._size_before
+                self._readable_size_after = self._readable_size_before
+            elif self._width_before == 1920 or self._height_before == 1080:
+                self._situation = "It will not change"
+                self._condition = "Dimensions are already standardized"
+                self._width_after = self._width_before
+                self._height_after = self._height_before
+                self._size_after = self._size_before
+                self._readable_size_after = self._readable_size_before
             else:
-                self.im = self.im.resize((1920, d.new_height()))
+                d = Dimensions(self._width_before, self._height_before)
 
-            self.im.save(full_file_path_resized())
+                if (self._width_before - 1920) > (self._height_before - 1080):
+                    self.im = self.im.resize((d.new_width(), 1080))
+                else:
+                    self.im = self.im.resize((1920, d.new_height()))
 
-            self._width_after, self._height_before = self.im.size
+                self.im.save(self.full_file_path_resized())
 
+                im_res = Image.open(self.full_file_path_resized())
+                self._width_after, self._height_before = im_res.size
+                self._size_after = self.image_size(self.full_file_path_resized())
+                self._readable_size_after = self.readable_size(self._size_after)
+                self._situation = "Changed file!"
+                self._condition = "Archive was in favorable conditions"
 
-class Dimension:
-
-    def __init__(self, width, height):
-        self._width = width
-        self._height = height
-
-    def new_height(self):
-        return int((self._height / self._width) * 1920)
-
-    def new_width(self):
-        return int((self._width / self._height) * 1080)
+    def change_report(self):
+        if self.file:
+            report = "File {}\n".format(self.full_file_path())
+            report = "{} - Dimensions (before):\n".format(report)
+            report = "{} {}| Width: {}\n".format(report, " "*4, self._width_before)
+            report = "{} {}| Height: {}\n".format(report, " "*4, self._height_before)
+            report = "{} - Dimensions: (after):\n".format(report)
+            report = "{} {}| Width: {}\n".format(report, " "*4, self._width_after)
+            report = "{} {}| Height: {}\n".format(report, " "*4, self._height_after)
+            report = "{} - Size:\n".format(report)
+            report = "{} {}| Before: {}\n".format(report, " "*4, self._readable_size_before)
+            report = "{} {}| After: {}\n".format(report, " "*4, self._readable_size_after)
+            report = "{} - Status:\n".format(report)
+            report = "{} {}| Situation: {}\n".format(report, " "*4, self._situation)
+            report = "{} {}| Condition: {}\n".format(report, " "*4, self._condition)
+            print(report)
